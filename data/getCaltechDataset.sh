e163e644b99d82df143b2c48d593ebee2d7d0bf1
Andrey Mulenkov <andymulx@gmail.com>
2017-07-31 04:59:53 +0300

    WIP

--- data/getCaltechDataset.sh
+++ data/getCaltechDataset.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
 tar xvzf 101_ObjectCategories.tar.gz
---
+++ data/getVisualizationData.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://c.yosinski.com/caffenet-yos_regularized_opt.tar.gz
+wget http://c.yosinski.com/caffenet-yos_max_im.tar.gz
+wget http://c.yosinski.com/caffenet-yos_max_deconv.tar.gz
